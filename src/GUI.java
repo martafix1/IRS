b3d4ac1290b790bcd53450613ebc91ae2d683162
m <martafix@gmail.com>
2019-01-26 13:09:38 +0100

    added gnuparameters work showcase

--- src/GUI.java
+++ src/GUI.java
@@ -13,16 +13,19 @@ public class GUI extends JFrame {
 
 
     private JMenuBar menuBar ;
-    private JMenu File;
-    private JMenuItem Exit;
-    private JMenu Settings;
-    private JMenu Help;
-    private JTextArea textPane;
-    private JPanel Displaypannel;
-    private JButton Button1;
+        private JMenu File;
+            private JMenuItem Exit;
+        private JMenu Settings;
+        private JMenu Help;
+        private JTextArea textPane;
+        private JPanel Displaypannel;
+        private JButton Button1;
+    private JToolBar Toolbar;
+        private  JButton TBBexit; //ToolBarButton
     /**-----------**/
     private PlotHandler plotHandler;
     private JPlot plot;
+    private boolean gnuplotNotFound;
     //private JMenu Help;
     private Dimension dimension;
     /*String gnupath = prop.getProperty("gnuplotpath");
@@ -41,12 +44,18 @@ public class GUI extends JFrame {
     public GUI(String title, Dimension dimension,String gnuplotpath) throws HeadlessException {
         super(title);
         this.dimension = dimension;
-        plotHandler =new PlotHandler(gnuplotpath);
+        try {
+            plotHandler =new PlotHandler(gnuplotpath);
+        }catch (Exception e){}
+
         menuBar = new JMenuBar();
         File = new JMenu("File");
         Settings = new JMenu("Settings");
         Help = new JMenu("Help");
         Exit = new JMenuItem("Exit");
+
+        Toolbar = new JToolBar();
+        TBBexit = new JButton(new ImageIcon("./prop/exit.png"));
         textPane = new JTextArea(10,30);
         Displaypannel = new JPanel();
         Button1 = new JButton("Click me");
@@ -73,15 +82,28 @@ public class GUI extends JFrame {
        });
        menuBar.add(Settings);
        menuBar.add(Help);
-        //tryPlot();
+
+
+       Toolbar.add(TBBexit);
+       TBBexit.addActionListener(new ActionListener() {
+           @Override
+           public void actionPerformed(ActionEvent e) {
+               exit();
+           }
+       });
+
+       //tryPlot();
         //plot = new JPlot(new JavaPlot("D:/Martin/Programovani/SOC/gnuplot/bin/gnuplot.exe", true));
         //plot.getJavaPlot().addPlot("x**2");
         //PlotHandler x=new PlotHandler("D:/Martin/Programovani/SOC/gnuplot/bin/gnuplot.exe");
 
-        Spectrum spc=new Spectrum("ahoj","./prop/a.txt");
-        plotHandler.addData(spc);
 
-        plotHandler.addPlot(0);
+
+
+        //Spectrum spc=new Spectrum("ahoj","./prop/a.txt");
+        //plotHandler.addData(spc);
+        plotHandler.addPlot("x**2");
+        //plotHandler.addPlot(0);
         plot = plotHandler.getGraph();
         //plot.setPreferredSize(new DimensionUIResource(100, 100));
         Displaypannel = new JPanel();
@@ -95,18 +117,23 @@ public class GUI extends JFrame {
     //this.getContentPane().add(plotHandler.getGraph());
        //Displaypannel.add(plotHandler.getGraph());
        this.getContentPane().setLayout(new BorderLayout());
+       this.getContentPane().add(Toolbar,BorderLayout.NORTH);
        this.getContentPane().add(new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,new JScrollPane(Displaypannel,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED),new JScrollPane(textPane,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED)),BorderLayout.CENTER);
       this.getContentPane().add(Button1,BorderLayout.SOUTH);
        //this.getContentPane().add(,BorderLayout.EAST);
         Button1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                plotHandler.addPlot("0.1");
+                plotHandler.addPlot("x**2");
                 plot = plotHandler.getGraph();
+                //plot.getJavaPlot().getParameters().getPage().;
                 Displaypannel.remove(0);
                 System.err.println("Clicked");
                 Displaypannel.add(plot);
                 GUI.super.getContentPane().revalidate();
+                System.out.println(plot.getJavaPlot().getCommands());
+                plot.getJavaPlot().getParameters().getPreInit().add("");
+                plot.getJavaPlot().getParameters().getPostInit().add("");
             }
         });
 
